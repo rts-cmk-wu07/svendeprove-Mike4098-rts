f02382f748cf4dbc6200d06d6596fcff8f489c5b
Mike <U10604@rts-365.dk>
2023-03-14 10:33:11 +0100

    Tailwind and other liberaries sat up

---
+++ mike-svendeproeve/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: '#EAEAEA',
+        darkPurple: '#5E2E53',
+        pink: '#E1A1E9',
+        black: '#000000',
+      },
+      fontSize: {
+        sm: '18px',
+        md: '24px',
+        lg: '36px'
+      }
+    },
+   
+  },
+  plugins: [],
+}

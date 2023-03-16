10d166bc7fe9a4b49488dfedf49fadacf3f1b6ef
Mike <U10604@rts-365.dk>
2023-03-16 12:11:50 +0100

    welcome

--- mike-svendeproeve/src/pages/Welcome.js
+++ mike-svendeproeve/src/pages/Welcome.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 const Welcome = () => {
     return ( 
-    <div className='h-screen w-screen bg-contain bg-no-repeat bg-splash'>
-        welcome
-        <button><Link to="/activities">Kom i gang</Link></button>
-    </div> );
+            <div className='h-screen flex items-end justify-center pb-12 bg-center w-screen bg-cover  bg-no-repeat bg-splash'>
+                <button className="bg-darkPurple w-64 h-14 text-md text-grey rounded-[10px]"><Link to="/activities">Kom i gang</Link></button>
+            </div> 
+    );
 }
  
 export default Welcome;
